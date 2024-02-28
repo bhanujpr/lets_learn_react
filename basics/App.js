9cbc1f16ae8b547754964c07a3cf15440aa57db0
Bhanu pratap <bhanurajawatt@gmail.com>
2024-02-28 12:04:07 +0530

    starting food villa

--- basics/App.js
+++ basics/App.js
@@ -43,9 +43,547 @@ const container= <div>{heading} {heading2}</div>
 //     <h1>Hello</h1>
 // }
 //for multiple line use simply (); write your code inside it
-const NewComponent=()=>{
-    return <div><h1>Hello</h1><h2>World</h2></div>;
+
+/**
+ * 
+ * Navbar
+ *      -Logo
+ *      -List
+ * Body
+ *      -search
+ *      -card
+ * Footer
+ * 
+ */
+
+const NavBar =()=>{
+    return(
+        <div className='navbar'>
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNYm8lAGJ3xhJtR97fPwyWwPce0uMOFXdRiQ&usqp=CAU'/>
+        <ul >
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+            <li>Cart</li>
+        </ul>
+        </div>
+    )
+}
+
+const restaurantList =[
+    {
+      "info": {
+        "id": "58312",
+        "name": "Pizza Hut",
+        "cloudinaryImageId": "2b4f62d606d1b2bfba9ba9e5386fabb7",
+        "locality": "Lake Mall",
+        "areaName": "Kalighat",
+        "costForTwo": "₹350 for two",
+        "cuisines": [
+          "Pizzas"
+        ],
+        "avgRating": 4.2,
+        "parentId": "721",
+        "avgRatingString": "4.2",
+        "totalRatingsString": "10K+",
+        "sla": {
+          "deliveryTime": 46,
+          "lastMileTravel": 3.7,
+          "serviceability": "SERVICEABLE",
+          "slaString": "45-50 mins",
+          "lastMileTravelString": "3.7 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+          "nextCloseTime": "2024-02-29 01:00:00",
+          "opened": true
+        },
+        "badges": {
+          
+        },
+        "isOpen": true,
+        "type": "F",
+        "badgesV2": {
+          "entityBadges": {
+            "imageBased": {
+              
+            },
+            "textBased": {
+              
+            },
+            "textExtendedBadges": {
+              
+            }
+          }
+        },
+        "aggregatedDiscountInfoV3": {
+          "header": "50% OFF",
+          "subHeader": "UPTO ₹100"
+        },
+        "differentiatedUi": {
+          "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          "differentiatedUiMediaDetails": {
+            "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+            "lottie": {
+              
+            },
+            "video": {
+              
+            }
+          }
+        },
+        "reviewsSummary": {
+          
+        },
+        "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        "restaurantOfferPresentationInfo": {
+          
+        }
+      },
+      "analytics": {
+        
+      },
+      "cta": {
+        "link": "https://www.swiggy.com/restaurants/pizza-hut-lake-mall-kalighat-kolkata-58312",
+        "type": "WEBLINK"
+      }
+    },
+    {
+      "info": {
+        "id": "13927",
+        "name": "Wow! Momo",
+        "cloudinaryImageId": "64fd45fd9f44c1737bc446e470bed666",
+        "locality": "Kasba",
+        "areaName": "Kasba",
+        "costForTwo": "₹300 for two",
+        "cuisines": [
+          "Tibetan",
+          "Healthy Food",
+          "Asian",
+          "Chinese",
+          "Snacks",
+          "Continental",
+          "Desserts",
+          "Beverages"
+        ],
+        "avgRating": 4.5,
+        "parentId": "1776",
+        "avgRatingString": "4.5",
+        "totalRatingsString": "10K+",
+        "sla": {
+          "deliveryTime": 24,
+          "lastMileTravel": 0.9,
+          "serviceability": "SERVICEABLE",
+          "slaString": "20-25 mins",
+          "lastMileTravelString": "0.9 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+          "nextCloseTime": "2024-02-28 23:59:00",
+          "opened": true
+        },
+        "badges": {
+          
+        },
+        "isOpen": true,
+        "type": "F",
+        "badgesV2": {
+          "entityBadges": {
+            "imageBased": {
+              
+            },
+            "textBased": {
+              
+            },
+            "textExtendedBadges": {
+              
+            }
+          }
+        },
+        "aggregatedDiscountInfoV3": {
+          "header": "60% OFF",
+          "subHeader": "UPTO ₹120"
+        },
+        "differentiatedUi": {
+          "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          "differentiatedUiMediaDetails": {
+            "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+            "lottie": {
+              
+            },
+            "video": {
+              
+            }
+          }
+        },
+        "reviewsSummary": {
+          
+        },
+        "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        "restaurantOfferPresentationInfo": {
+          
+        }
+      },
+      "analytics": {
+        
+      },
+      "cta": {
+        "link": "https://www.swiggy.com/restaurants/wow-momo-kasba-kolkata-13927",
+        "type": "WEBLINK"
+      }
+    },
+    {
+      "info": {
+        "id": "3022",
+        "name": "Aminia Restaurant",
+        "cloudinaryImageId": "ub35uaf2iferroprejgy",
+        "locality": "Golpark",
+        "areaName": "Golpark",
+        "costForTwo": "₹500 for two",
+        "cuisines": [
+          "Biryani",
+          "Mughlai",
+          "Indian",
+          "Kebabs",
+          "Tandoor"
+        ],
+        "avgRating": 4.5,
+        "parentId": "47",
+        "avgRatingString": "4.5",
+        "totalRatingsString": "10K+",
+        "sla": {
+          "deliveryTime": 31,
+          "lastMileTravel": 2.3,
+          "serviceability": "SERVICEABLE",
+          "slaString": "30-35 mins",
+          "lastMileTravelString": "2.3 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+          "nextCloseTime": "2024-02-29 01:00:00",
+          "opened": true
+        },
+        "badges": {
+          
+        },
+        "isOpen": true,
+        "aggregatedDiscountInfoV2": {
+          
+        },
+        "type": "F",
+        "badgesV2": {
+          "entityBadges": {
+            "imageBased": {
+              
+            },
+            "textBased": {
+              
+            },
+            "textExtendedBadges": {
+              
+            }
+          }
+        },
+        "differentiatedUi": {
+          "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          "differentiatedUiMediaDetails": {
+            "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+            "lottie": {
+              
+            },
+            "video": {
+              
+            }
+          }
+        },
+        "reviewsSummary": {
+          
+        },
+        "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        "restaurantOfferPresentationInfo": {
+          
+        }
+      },
+      "analytics": {
+        
+      },
+      "cta": {
+        "link": "https://www.swiggy.com/restaurants/aminia-restaurant-golpark-kolkata-3022",
+        "type": "WEBLINK"
+      }
+    },
+    {
+      "info": {
+        "id": "383830",
+        "name": "KFC",
+        "cloudinaryImageId": "f01666ac73626461d7455d9c24005cd4",
+        "locality": "Rajdanga Main Road",
+        "areaName": "Acropolis Mall",
+        "costForTwo": "₹400 for two",
+        "cuisines": [
+          "Burgers",
+          "Biryani",
+          "American",
+          "Snacks",
+          "Fast Food"
+        ],
+        "avgRating": 4.5,
+        "parentId": "547",
+        "avgRatingString": "4.5",
+        "totalRatingsString": "1K+",
+        "sla": {
+          "deliveryTime": 23,
+          "lastMileTravel": 1.3,
+          "serviceability": "SERVICEABLE",
+          "slaString": "20-25 mins",
+          "lastMileTravelString": "1.3 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+          "nextCloseTime": "2024-02-28 23:00:00",
+          "opened": true
+        },
+        "badges": {
+          
+        },
+        "isOpen": true,
+        "type": "F",
+        "badgesV2": {
+          "entityBadges": {
+            "imageBased": {
+              
+            },
+            "textBased": {
+              
+            },
+            "textExtendedBadges": {
+              
+            }
+          }
+        },
+        "aggregatedDiscountInfoV3": {
+          "header": "20% OFF",
+          "subHeader": "UPTO ₹50"
+        },
+        "differentiatedUi": {
+          "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          "differentiatedUiMediaDetails": {
+            "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+            "lottie": {
+              
+            },
+            "video": {
+              
+            }
+          }
+        },
+        "reviewsSummary": {
+          
+        },
+        "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        "restaurantOfferPresentationInfo": {
+          
+        }
+      },
+      "analytics": {
+        
+      },
+      "cta": {
+        "link": "https://www.swiggy.com/restaurants/kfc-rajdanga-main-road-acropolis-mall-kolkata-383830",
+        "type": "WEBLINK"
+      }
+    },
+    {
+      "info": {
+        "id": "216675",
+        "name": "Burger King",
+        "cloudinaryImageId": "e33e1d3ba7d6b2bb0d45e1001b731fcf",
+        "locality": "Kasba",
+        "areaName": "East Kolkata Township",
+        "costForTwo": "₹350 for two",
+        "cuisines": [
+          "Burgers",
+          "American"
+        ],
+        "avgRating": 4.4,
+        "parentId": "166",
+        "avgRatingString": "4.4",
+        "totalRatingsString": "10K+",
+        "sla": {
+          "deliveryTime": 25,
+          "lastMileTravel": 1.2,
+          "serviceability": "SERVICEABLE",
+          "slaString": "25-30 mins",
+          "lastMileTravelString": "1.2 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+          "nextCloseTime": "2024-02-28 23:00:00",
+          "opened": true
+        },
+        "badges": {
+          
+        },
+        "isOpen": true,
+        "type": "F",
+        "badgesV2": {
+          "entityBadges": {
+            "imageBased": {
+              
+            },
+            "textBased": {
+              
+            },
+            "textExtendedBadges": {
+              
+            }
+          }
+        },
+        "aggregatedDiscountInfoV3": {
+          "header": "60% OFF",
+          "subHeader": "UPTO ₹120"
+        },
+        "differentiatedUi": {
+          "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          "differentiatedUiMediaDetails": {
+            "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+            "lottie": {
+              
+            },
+            "video": {
+              
+            }
+          }
+        },
+        "reviewsSummary": {
+          
+        },
+        "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        "restaurantOfferPresentationInfo": {
+          
+        }
+      },
+      "analytics": {
+        
+      },
+      "cta": {
+        "link": "https://www.swiggy.com/restaurants/burger-king-kasba-east-kolkata-township-kolkata-216675",
+        "type": "WEBLINK"
+      }
+    },
+    {
+      "info": {
+        "id": "29424",
+        "name": "Momo I Am",
+        "cloudinaryImageId": "vuygl4mrkquhg0ma5dqv",
+        "areaName": "Golpark",
+        "costForTwo": "₹350 for two",
+        "cuisines": [
+          "Asian",
+          "Chinese",
+          "Tibetan"
+        ],
+        "avgRating": 4.6,
+        "parentId": "2055",
+        "avgRatingString": "4.6",
+        "totalRatingsString": "10K+",
+        "sla": {
+          "deliveryTime": 36,
+          "lastMileTravel": 2.1,
+          "serviceability": "SERVICEABLE",
+          "slaString": "35-40 mins",
+          "lastMileTravelString": "2.1 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+          "nextCloseTime": "2024-02-28 23:00:00",
+          "opened": true
+        },
+        "badges": {
+          
+        },
+        "isOpen": true,
+        "type": "F",
+        "badgesV2": {
+          "entityBadges": {
+            "imageBased": {
+              
+            },
+            "textBased": {
+              
+            },
+            "textExtendedBadges": {
+              
+            }
+          }
+        },
+        "aggregatedDiscountInfoV3": {
+          "header": "40% OFF",
+          "subHeader": "UPTO ₹80"
+        },
+        "differentiatedUi": {
+          "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          "differentiatedUiMediaDetails": {
+            "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+            "lottie": {
+              
+            },
+            "video": {
+              
+            }
+          }
+        },
+        "reviewsSummary": {
+          
+        },
+        "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        "restaurantOfferPresentationInfo": {
+          
+        }
+      },
+      "analytics": {
+        
+      },
+      "cta": {
+        "link": "https://www.swiggy.com/restaurants/momo-i-am-golpark-kolkata-29424",
+        "type": "WEBLINK"
+      }
+    }]
+
+
+const Card=({
+    name,
+    cuisines,
+    avgRating,
+    cloudinaryImageId
+})=>{
+    return(
+        <div className='card'>
+            <img src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"+cloudinaryImageId }/>
+            <h2>{name}</h2>
+            <h3>{cuisines.join(', ')}</h3>
+            <h4>{avgRating} average rating</h4>
+        </div>
+    )
+}
+const Body=()=>{
+    return(
+        <div className='restaurant-list'>{
+            restaurantList.map((restaurant)=> {
+                return <Card {...restaurant.info} key={restaurant.info.id}/>
+            })
+        }
+        </div>
+    )
+}
+const AllComponents =()=>{
+    return(
+        <>
+        <NavBar/>
+        <Body/>
+        </>
+    )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<NewComponent/>);
+root.render(<AllComponents/>);
