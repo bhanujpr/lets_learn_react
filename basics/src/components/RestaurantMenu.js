dbf9eaa04723103753821c74eefc408f7d3b170d
Bhanu pratap <bhanurajawatt@gmail.com>
2024-03-02 22:44:08 +0530

    .

--- basics/src/components/RestaurantMenu.js
+++ basics/src/components/RestaurantMenu.js
@@ -1,7 +1,29 @@
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+
+
+
 const RestaurantMenu=()=>{
     const params=useParams();
     const {id}=params;
+    const [restaurant,setRestaurant]=useState(null);
+
+
+    useEffect(()=>{
+        getRestaurantMenu();
+    });
+    
+    async function getRestaurantMenu(){
+        const data = await fetch("https://foodfire.onrender.com/api/menu?page-type=REGULAR_MENU&complete-menu=true&lat=21.1702401&lng=72.83106070000001&&submitAction=ENTER&restaurantId="+id);
+        const json=await data.json();
+        setRestaurant(json?.data?.cards?.card?.info);
+         console.log(restaurant);
+    }
+
+
+
+
     return(
         <div>
             <h1>Restaurant id:{id}</h1>
